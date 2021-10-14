c01349d9658308cec5e8c452a08044d88c9425c0
Бурбас Иван <Icon003@yandex.by>
2021-10-15 02:43:16 +0300

    Создание компонента поля ввода

---
+++ src/components/AppInput.vue
@@ -0,0 +1,46 @@
+<template>
+	<input
+		class="app-input"
+		type="text"
+		:placeholder="placeholder"
+		@input="callback($event)"
+	/>
+</template>
+
+<script>
+export default {
+	props: {
+		placeholder: {
+			type: String,
+			require: false,
+			default: "Placeholder"
+		}
+	},
+	methods: {
+		callback(e) {
+			this.$emit("input", e.target.value);
+		}
+	}
+}
+</script>
+
+<style scoped>
+	.app-input {
+		width: 100%;
+		padding-top: 8px;
+		padding-right: 25.5%;
+		padding-bottom: 8px;
+		padding-left: 25.5%;
+
+		font-family: "Roboto";
+		font-size: 18px;
+		line-height: 22px;
+
+		border: 1px solid var(--border-input-color);
+		background: transparent;
+	}
+	.app-input::placeholder {
+		color: var(--text-color);
+	}
+
+</style>

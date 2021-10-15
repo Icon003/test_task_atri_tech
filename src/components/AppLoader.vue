69d77110f38c9f53b8043d935720b9ca5d0dc47c
Бурбас Иван <Icon003@yandex.by>
2021-10-15 03:45:35 +0300

    Создание компонента лоадера

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,50 @@
+<template>
+	<transition name="fade">
+		<div v-if="isLoading" class="app-loader">
+			<img
+				class="app-loader__image"
+				src="@/assets/loaders/puff.svg"
+				alt="Загрузка"
+			/>
+		</div>
+	</transition>
+</template>
+
+<script>
+export default {
+	props: {
+		isLoading: {
+			type: Boolean,
+			require: true
+		}
+	}
+}
+</script>
+
+<style scoped>
+.fade-enter-active,.fade-leave-active {
+	transition: opacity 0.5s;
+}
+.fade-enter,.fade-leave-to {
+	opacity: 0;
+}
+.app-loader {
+	display: flex;
+
+	justify-content: center;
+	align-items: center;
+
+	width: 100%;
+	height: 100vh;
+
+	position: fixed;
+	z-index: 3;
+
+	background-color: var(--loader-background-color);
+}
+.app-loader__image {
+	width: 1200px;
+	height: 120px;
+}
+
+</style>
